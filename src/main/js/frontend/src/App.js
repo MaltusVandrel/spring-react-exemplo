8ba02e00b0e064863dfd4a247233813bcb26b1cb
Matheus <Matheus@DESKTOP-5LSHH6H>
2019-05-27 00:11:29 -0300

    att react responsivness

--- src/main/js/frontend/src/App.js
+++ src/main/js/frontend/src/App.js
@@ -7,22 +7,43 @@ import "./App.css";
 
 				
 class TableHeader extends React.Component {
- properties={};
- constructor(props) {
+iconHasAsc = <span class="material-icons">keyboard_arrow_down</span>; 
+iconHasDesc = <span class="material-icons">keyboard_arrow_up</span>; 
+iconHasent = <span class="material-icons">unfold_more</span>; 
+constructor(props) {
 	super(props);
- }   
+	 this.handleClick = this.handleClick.bind(this);
+	 this.state = {
+      icon : this.iconHasent
+    };
+ }  
+ handleClick(e) {
+	 e.preventDefault();
+    
+    console.log('The link was clicked.');
+    this.props.parent.order(this.props.property);
+    if(this.props.parent.has(this.props.property,'ASC')){
+    	this.setState({
+    		 icon :	this.iconHasAsc
+        });
+    }else if(this.props.parent.has(this.props.property,'DESC')){
+    	this.setState({
+    		 icon :	this.iconHasDesc
+        });
+    }else{
+    	this.setState({
+    		 icon :	this.iconHasent
+        });
+    }   
+ }
  render() {
-    let icon; 
-	if(this.props.parent.has('id','ASC')){
-		icon = <span class="material-icons">keyboard_arrow_down</span>;
-	}else if(this.props.parent.has('id','DESC')){
-		icon = <span class="material-icons">keyboard_arrow_up</span>;
-	}else{
-		icon = <span class="material-icons">unfold_more</span>;
-	}
+   
+	let has = this.props.parent.has;
+	let prop = this.props.property;
     return (
-      <th onClick={this.props.parent.order('id')}>
-		  {this.props.header} {icon}
+      <th onClick={this.handleClick}>
+		  {this.props.header} 
+		  {this.state.icon}
 	  </th>
     );
   }
@@ -45,61 +66,86 @@ class TableBody extends Component {
 		super(props);
 	}	
 	render() {
-		let content = <tr>;
-		for(row of this.props.rows){
+		let content = "";
+		if(this.props.rows){
+		for(let i=0;i<this.props.rows.length;i++){
 //			for(data of Object.keys(row)){
 //				content+= <Cell value={data} />
 //			}
 	    }
-		content+=</tr>;
-	    return (
-	    		{content}
-	    )
+//		content+=;
+		}
+	    return 	(<tr>{content}</tr>);
+	    
 	}
 }
 class App extends Component {
   rows = [];
   page = {}
 	  
-   grid={
-		perPage:20,
-		page:0,
-		object:{titulo:""},
-		sort:{orders:[]}
+  grid={
+	perPage:20,
+	page:0,
+	object:{titulo:""},
+	sort:{orders:[]}
+  }
+     async getGrid (doKeepPage){
+		if(!(doKeepPage)){
+			this.grid.object.page=0;
+		}
+		if(this.grid.object.id<0||this.grid.object.id=="")this.grid.object.id=undefined;
+		if(this.grid.object.titulo==="")this.grid.object.titulo=undefined;
+		if(this.grid.object.descricao==="")this.grid.object.descricao=undefined;
+		if(this.grid.object.abertura==="")this.grid.object.abertura=undefined;
+		if(this.grid.object.fechamento=="")this.grid.object.fechamento=undefined;
+		if(this.grid.object.situacao=="")this.grid.object.situacao=undefined;
+		
+		const response = await fetch(new Request("http://localhost:8080/grid",{ method: 'POST',
+	        headers: {'Content-Type':"application/json"},
+	        mode: 'cors',
+	        cache: 'default',
+	        body:JSON.stringify(this.grid)}));
+	    
+	    
+	    this.page = await response.json();
 	}
-
 	order=function(property){
-		for(let x in this.grid.sort){
-			if(this.grid.sort[x].property===property){
-				if(this.grid.sort[x].direction==='ASC'){
-					this.grid.sort[x].direction='DESC';
+		for(let x in this.grid.sort.orders){
+			if(this.grid.sort.orders[x].property===property){
+				if(this.grid.sort.orders[x].direction==='ASC'){
+					this.grid.sort.orders[x].direction='DESC';
 					this.getGrid(true);
 					return;
 				}else{
-					this.grid.sort.splice(x,1);
+					this.grid.sort.orders.splice(x,1);
 					this.getGrid(true);
 					return;
 				}
 			}
 		}
-		this.grid.sort.push({property:property,direction:'ASC'});
-		this.getGrid(true);		
+		this.grid.sort.orders.push({property:property,direction:'ASC'});
+		
+		this.getGrid(true);
+		
 	};
 
 	has=function(property,direction){
-		for(let x in this.grid.sort){
-			if(this.grid.sort[x].property===property){
-				if(direction){
-					if(this.grid.sort[x].direction===direction){
-						return true;
-					}else{
-						return false;
+		console.log(JSON.stringify("One1"));
+		if(this){
+			for(let x in this.grid.sort.orders){
+				if(this.grid.sort.orders[x].property===property){
+					if(direction){
+						if(this.grid.sort.orders[x].direction===direction){
+							return true;
+						}else{
+							return false;
+						}
 					}
+					return true;
 				}
-				return true;
 			}
+			return false;
 		}
-		return false;
 	}
 
 	first=function(){
@@ -120,18 +166,6 @@ class App extends Component {
   
   
   
-  async componentDidMount() {
-	  
-    const response = await fetch(new Request("http://localhost:8080/grid",{ method: 'POST',
-        headers: {'Content-Type':"application/json"},
-        mode: 'cors',
-        cache: 'default',
-        body:JSON.stringify(this.grid)}));
-    
-    
-    this.page = await response.json();
-    
-  }
   render() {
     return (
     		<table>
@@ -145,12 +179,12 @@ class App extends Component {
     					<th><input type='text' placeholder='Situacao'/></th>
     				</tr>
     				<tr>
-	    				<TableHeader parent={this} header={"ID"} />
-						<TableHeader parent={this} header={"Titulo"} />
-						<TableHeader parent={this} header={"Descricao"} />
-						<TableHeader parent={this} header={"Abertura"} />
-						<TableHeader parent={this} header={"Fechamento"} />
-						<TableHeader parent={this} header={"Situacao"} />		
+	    				<TableHeader parent={this} property={"id"} header={"ID"} />
+						<TableHeader parent={this} property={"titulo"} header={"Titulo"} />
+						<TableHeader parent={this} property={"descricao"} header={"Descricao"} />
+						<TableHeader parent={this} property={"abertura"} header={"Abertura"} />
+						<TableHeader parent={this} property={"fechamento"} header={"Fechamento"} />
+						<TableHeader parent={this} property={"situacao"} header={"Situacao"} />		
     				</tr>
     			</thead>
     			<tbody>
